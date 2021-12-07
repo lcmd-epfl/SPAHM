c4780684cb8e5e6d6d59d234dd4df0645cb1ace2
Ksenia <ksenia.briling@epfl.ch>
2021-12-07 16:53:46 +0100

    Compute and save kernel

---
+++ code/32_kernel.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+import numpy as np
+from sklearn.model_selection import train_test_split
+from utils import get_kernel
+
+parser = argparse.ArgumentParser(description='This program computes kernel.')
+parser.add_argument('--x',      type=str,   dest='repr',      required=True, help='path to the representations file')
+parser.add_argument('--sigma',  type=float, dest='sigma',     default=32.0,  help='sigma hyperparameter (default=32.0)')
+parser.add_argument('--kernel', type=str,   dest='kernel',    default='L',   help='kernel type (G for Gaussian, L for Laplacian, myL for Laplacian for open-shell systems) (default L)')
+parser.add_argument('--dir',    type=str,   dest='dir',       default='./',   help='directory to save the output in (default=current dir)')
+args = parser.parse_args()
+print(vars(args))
+
+def main():
+  X = np.load(args.repr)
+  sigma = args.sigma
+  kernel = get_kernel(args.kernel)
+  K = kernel(X, X, 1.0/args.sigma)
+  np.save(args.dir+'/K_'+args.kernel, K)
+
+if __name__ == "__main__":
+  main()
+
